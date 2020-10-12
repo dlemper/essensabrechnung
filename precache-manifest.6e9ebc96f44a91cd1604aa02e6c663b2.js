dcbf13dd392bbce3dfd40f4280a26bfb788d1429
Daniel Lemper <ich@dlemper.de>
2020-10-12 09:38:18 +0200

    deploy

---
+++ precache-manifest.6e9ebc96f44a91cd1604aa02e6c663b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f654715d3d8e81bc3d5",
+    "url": "/essensabrechnung/css/app.6011255d.css"
+  },
+  {
+    "revision": "ba5d9ef69458815f79ecf1b057a8455b",
+    "url": "/essensabrechnung/index.html"
+  },
+  {
+    "revision": "3f654715d3d8e81bc3d5",
+    "url": "/essensabrechnung/js/app.604e17ec.js"
+  },
+  {
+    "revision": "398e6fd56704472f9397",
+    "url": "/essensabrechnung/js/chunk-vendors.0fa7ce91.js"
+  },
+  {
+    "revision": "c8629d15b05740be59f417c7ecc1cd49",
+    "url": "/essensabrechnung/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/essensabrechnung/robots.txt"
+  }
+]);
